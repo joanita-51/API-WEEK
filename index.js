d9237dde31f6b4330ca4497e32944c89011e7990
NAKITYOJOANITA <22110043-NAKITYOJOANITA@users.noreply.replit.com>
2023-04-09 11:23:20 +0000

    Fetching data for a specific user and error handling

--- index.js
+++ index.js
@@ -9,12 +9,25 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res)=>{
-  res.status(200).send("Job Board API")
+
+app.get("/job/:id",(req, res)=>{
+  const id = parseInt(req.params.id)
+  const response = jobData[id]
+  if(response){
+    res.status(200).send(response);
+  }else{
+    res.status(400).send("No matching id found")
+  }
+
 })
+
 
 app.get("/job",(req, res)=>{
   res.status(200).send(jobData)
+})
+
+app.get('/', (req, res)=>{
+  res.status(200).send("Job Board API")
 })
 
 app.listen(port, () => {
